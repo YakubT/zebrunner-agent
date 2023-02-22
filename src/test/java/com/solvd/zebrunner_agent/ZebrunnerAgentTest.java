04489136552695a0288ef6496069e1786c1ad7e6
Anatolii Yakubyshyn <goldyakub@gmail.com>
2023-02-22 13:34:14 +0200

    Fixed code convention mistakes

--- src/test/java/com/solvd/zebrunner_agent/ZebrunnerAgentTest.java
+++ src/test/java/com/solvd/zebrunner_agent/ZebrunnerAgentTest.java
@@ -1,10 +1,11 @@
 package com.solvd.zebrunner_agent;
 
-import com.solvd.zebrunner_agent.enums.TestStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import com.solvd.zebrunner_agent.enums.TestStatus;
 
 public class ZebrunnerAgentTest {
 
